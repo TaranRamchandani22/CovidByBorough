96bcba96681e8097d2e2afdce1f07a127807f4d5
amelia-dal <Amelia.dalvito22@trinityschoolnyc.org>
2021-11-17 12:56:00 -0500

    notes.js

---
+++ notes.js
@@ -0,0 +1,17 @@
+/*
+----NOTES----
+--walking skeleton--
+a walking skeleton is to build the internal architecture
+  -your goal is to save the infographic for later,
+    -and make sure the other parts of your website work
+--folder--
+  -check out fs.demo.js to figure out how to do generateJSON.js
+source folder
+  -holds all of your images, and blank templates
+
+--To-Do--
+-come up with a naming convention
+  -use looping to do that
+    -makes things easier
+-use looping to come up with html files
+*/
