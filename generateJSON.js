8d9f594fb2775b864113a905eee9f55b6a060d02
amelia-dal <Amelia.dalvito22@trinityschoolnyc.org>
2021-11-19 11:05:10 -0500

    generateJSON.js is complete
    **We just need to put the right files in the right folder, so we would need to change the folder path in this file

--- generateJSON.js
+++ generateJSON.js
@@ -2,25 +2,28 @@ const fs = require('fs');
 
 let neighborhoods = [];
 
-//let peeps_csv = fs.readFileSync('data/Characters.csv', 'utf8');
-
-//let peeps = peeps_csv.split("\n");
+let peeps_csv = fs.readFileSync('data-by-modzcta.cvs', 'utf8');
+let peeps = peeps_csv.split("\n");
 
 peeps.forEach(function(peep) {
   let neighborhood_info = peep.split(';');//split based on the ;
   let neighborhood = {};
   neighborhood['zip'] = neighborhood_info[1];
-  neighborhood['income'] = neighborhood_info[2];
-  neighborhood['numCases'] = neighborhood_info[3];
-  neighborhood['caseRate'] = neighborhood_info[4];
-  neighborhood['numVacinations'] = neighborhood_info[5];
-  neighborhood['vacinationRate'] = neighborhood_info[5];
-
+  neighborhood['allHouseholdIncome'] = neighborhood_info[2];
+  neighborhood['caseRate'] = neighborhood_info[3];
+  neighborhood['vacinationRate'] = neighborhood_info[4];
+  neighborhood['allHouseholdIncome'] = neighborhood_info[5];
+  neighborhood['percentLatino'] = neighborhood_info[6];
+  neighborhood['percentAsianNonHispanic'] = neighborhood_info[7];
+  neighborhood['percentWhiteNonHispanic'] = neighborhood_info[8];
+  neighborhood['percentOtherEthnicity'] = neighborhood_info[9];
+  neighborhood['percentEthnicityUnkown'] = neighborhood_info[10];
 /*
 ***HOW ARE WE HANDLING RACE?
 should we do an if statement if a certain neiborhood does not contain a certain race percentage?
   */
-
+  neighborhoods.push(neighborhood);
 });
+fs.writeFileSync('data-by-modzcta.json', JSON.stringify(neighborhoods), 'utf8');//we shoulf puy yhid in the data file
 
 //fs.writeFileSync('data/potter.json', JSON.stringify(characters), 'utf8');
