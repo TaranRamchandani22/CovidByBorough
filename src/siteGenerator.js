87f169ec9160a0e55aa19432c0dd6f8abdd72f50
TaranRamchandani22 <taran.ramchandani22@trinityschoolnyc.org>
2021-11-27 21:28:55 -0500

    site generator

--- src/siteGenerator.js
+++ src/siteGenerator.js
@@ -1,12 +1,35 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let neiborhood_info = fs.readFileSync('../data/totalData.json', 'utf8');//1. read JSON object and store it in character info
-let index_template = fs.readFileSync('views/index.ejs', 'utf8');//2. reads views/index.ejs, and in the file, there is js that outputs the data in the propper way, loads template
+let displayData = fs.readFileSync('../../data/totalData.json', 'utf8');
+let siteTemplate = fs.readFileSync('index.ejs', 'utf8');
+let arrayZipCodes = ["10001,10002,10003,10004,10005,10006,10007,10009,10010,10011,10012,10013,10014,10016,10017,10018,10019,10021,10022,10023,10024,10025,10026,10027,10028,10029,10030,10031,10032,10033,10034,10035,10036,10037,10038,10039,10040,10044,10065,10069,10075,10128,10280,10282"];
+let newDisplayData = [];
 
-let index_html = ejs.render(index_template, {//renders data into template
-  filename: __dirname + '/views/index.ejs',
-  data: JSON.parse(neiborhood_info)
+arrayZipCodes.forEach(function(currentI,index){
+  newDisplayData.push(fs.readFileSync('../../data/'+currentI+'.json', 'utf8'))
+});
+let indexHTML = ejs.render(siteTemplate, {
+  filename: __dirname + 'index.ejs',
+  data: JSON.parse(displayData)
 });
 
-fs.writeFileSync('../build/index.html', index_html, 'utf8'); //saves the index.html file
+fs.writeFileSync('../../build/index.html', indexHTML, 'utf8');
+
+let microTemplate = fs.readFileSync('micro.ejs', 'utf8');
+
+for(i of arrayOfStateNames){
+  let microdisplayData = fs.readFileSync('../../data/'+i+'.json', 'utf8');
+  let microHTML = ejs.render(microTemplate, {
+    filename: __dirname + 'micro.ejs',
+    data: JSON.parse(microdisplayData)
+  });
+    fs.writeFileSync('../../build/' + [i] + '.html', microHTML, 'utf8');
+}
+
+
+let aboutTemplate = fs.readFileSync('about.ejs', 'utf8');
+let aboutHTML = ejs.render(aboutTemplate, {
+  filename: __dirname + 'about.ejs',
+});
+fs.writeFileSync('../../build/about.html', aboutHTML, 'utf8');
