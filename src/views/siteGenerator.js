742136777d089449cc3cbd45ac843ff69705ccdb
TaranRamchandani22 <taran.ramchandani22@trinityschoolnyc.org>
2021-11-29 21:40:07 -0500

    updated site generation

--- src/views/siteGenerator.js
+++ src/views/siteGenerator.js
@@ -2,23 +2,35 @@ const fs = require('fs');
 const ejs = require('ejs');
 
 let displayData = fs.readFileSync('../../data/totalData.json', 'utf8');
-let siteTemplate = fs.readFileSync('index.ejs', 'utf8');
+let indexTemplate = fs.readFileSync('index.ejs', 'utf8');
+let aboutTemplate = fs.readFileSync('about.ejs', 'utf8');
+let microTemplate = fs.readFileSync('micro.ejs', 'utf8');
 let arrayZipCodes = [10001,10002,10003,10004,10005,10006,10007,10009,10010,10011,10012,10013,10014,10016,10017,10018,10019,10021,10022,10023,10024,10025,10026,10027,10028,10029,10030,10031,10032,10033,10034,10035,10036,10037,10038,10039,10040,10044,10065,10069,10075,10128,10280,10282];
 let newDisplayData = [];
 
+/*
 arrayZipCodes.forEach(function(i,index){
 //for (i of arrayZipCodes) {
   newDisplayData.push(fs.readFileSync('../../data/' + i + '.json', 'utf8'))
 });
-
+*/
 
 let indexHTML = ejs.render(siteTemplate, {
   filename: __dirname + 'index.ejs',
   data: JSON.parse(displayData)
 });
-fs.writeFileSync('../../build/index.html', indexHTML, 'utf8');
 
-let microTemplate = fs.readFileSync('micro.ejs', 'utf8');
+let aboutHTML = ejs.render(siteTemplate, {
+  filename: __dirname + 'about.ejs',
+});
+
+for(i of arrayZipCodes){
+  let microHTML = ejs.render(microTemplate, {
+    filename: __dirname + '/views/micro.ejs',
+  });
+  fs.writeFileSync('../build/'+i+'.html',microHTML,'utf8')
+}
+/*
 for (i of arrayZipCodes) {
   let microData = fs.readFileSync('../../data/' + i + '.json', 'utf8');
   let microHTML = ejs.render(microTemplate, {
@@ -27,9 +39,6 @@ for (i of arrayZipCodes) {
   });
   fs.writeFileSync('../../build/' + [i] + '.html', microHTML, 'utf8');
 }
-
-let aboutTemplate = fs.readFileSync('about.ejs', 'utf8');
-let aboutHTML = ejs.render(aboutTemplate, {
-  filename: __dirname + 'about.ejs',
-});
+*/
+fs.writeFileSync('../../build/index.html', indexHTML, 'utf8');
 fs.writeFileSync('../../build/about.html', aboutHTML, 'utf8');
