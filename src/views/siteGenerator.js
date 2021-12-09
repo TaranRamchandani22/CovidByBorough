9450296aa4664c943953767e5adb77ae26a722f4
TaranRamchandani22 <taran.ramchandani22@trinityschoolnyc.org>
2021-12-09 15:06:43 -0500

    prebuild js

--- src/views/siteGenerator.js
+++ src/views/siteGenerator.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let displayImage1 = fs.readFileSync('../../src/images/IMG_7102.JPG');
-let displayImage2 = fs.readFileSync('../../src/images/IMG_7103.JPG');
-let displayImage3 = fs.readFileSync('../../src/images/TaranMacro.jpg');
-let displayImage4 = fs.readFileSync('../../src/images/TaranMicro.jpg');
+let displayImage1 = fs.readFileSync('../images/IMG_7102.JPG');
+let displayImage2 = fs.readFileSync('../images/IMG_7103.JPG');
+let displayImage3 = fs.readFileSync('../images/TaranMacro.jpg');
+let displayImage4 = fs.readFileSync('../images/TaranMicro.jpg');
 
 let displayData = fs.readFileSync('../../data/totalData.json', 'utf8');
 let indexTemplate = fs.readFileSync('index.ejs', 'utf8');
